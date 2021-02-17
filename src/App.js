da0ffad295ac8ac8e7367033e1c88665494fe0d4
userlab-luisdeleon <59143067+luisleon-userlab@users.noreply.github.com>
2021-02-16 19:22:09 -0600

    update generic table
    generic table for reports section

--- src/App.js
+++ src/App.js
@@ -5,16 +5,15 @@ import { createBrowserHistory } from 'history'
 import AppRouter from './Router'
 import Login from './pages/login/Login'
 import { Context } from './context'
-
 const history = createBrowserHistory()
 
 function App() {
   const [{ users }] = useContext(Context)
 
-  // if (!users.id) {
-  //   history.push('/')
-  //   return <Login />
-  // }
+  if (!users.id) {
+    history.push('/')
+    return <Login />
+  }
 
   return (
     <Router history={history}>
