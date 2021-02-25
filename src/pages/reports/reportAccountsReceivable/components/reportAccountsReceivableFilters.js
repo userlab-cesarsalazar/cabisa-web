37e630252a7b1be4f56886625e80f877f83ec15c
Luis de leon <luisdeleon_525@hotmail.com>
2021-02-24 18:26:03 -0600

    update
    update

--- src/pages/reports/reportAccountsReceivable/components/reportAccountsReceivableFilters.js
+++ src/pages/reports/reportAccountsReceivable/components/reportAccountsReceivableFilters.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import {Col, DatePicker, Input, Row, Select, Tag} from "antd";
-import SearchOutlined from "@ant-design/icons/lib/icons/SearchOutlined";
+import { Col, DatePicker, Input, Row, Select, Tag } from 'antd'
+import SearchOutlined from '@ant-design/icons/lib/icons/SearchOutlined'
 
 const { Search } = Input
 const { Option } = Select
+const { RangePicker } = DatePicker
 
 function ReportAccountsReceivableFilters() {
+<<<<<<< Updated upstream
   
   return(
     <>
@@ -61,5 +63,63 @@ function ReportAccountsReceivableFilters() {
   </Row>
       </>)
     
+=======
+  return (
+    <>
+      <Row gutter={16}>
+        <Col xs={6} sm={6} md={6} lg={6}>
+          <Search
+            prefix={<SearchOutlined className={'cabisa-table-search-icon'} />}
+            placeholder='Presiona enter para buscar'
+            className={'cabisa-table-search customSearch'}
+            size={'large'}
+          />
+        </Col>
+        <Col xs={6} sm={6} md={6} lg={6}>
+          <DatePicker
+            style={{ width: '100%', height: '40px', borderRadius: '8px' }}
+          />
+        </Col>
+        <Col xs={6} sm={6} md={6} lg={6}>
+          <Select
+            showSearch
+            className={'single-select'}
+            placeholder={'Tipo cliente'}
+            size={'large'}
+            style={{ width: '100%', height: '40px' }}
+            getPopupContainer={trigger => trigger.parentNode}
+          >
+            <Option value={0}>Persona Individual</Option>
+            <Option value={1}>Empresa</Option>
+          </Select>
+        </Col>
+        <Col xs={6} sm={6} md={6} lg={6}>
+          <DatePicker placeholder={'Fecha de Facturacion'} />
+        </Col>
+      </Row>
+      <Row gutter={16}>
+        <Col xs={6} sm={6} md={6} lg={6}>
+          <DatePicker placeholder={'Fecha de Pago'} />
+        </Col>
+        <Col xs={6} sm={6} md={6} lg={6}>
+          <DatePicker placeholder={'Fecha de Pago realizado'} />
+        </Col>
+        <Col xs={6} sm={6} md={6} lg={6}>
+          <Select
+            showSearch
+            className={'single-select'}
+            placeholder={'Situacion'}
+            size={'large'}
+            style={{ width: '100%', height: '40px' }}
+            getPopupContainer={trigger => trigger.parentNode}
+          >
+            <Option value={0}>Persona Individual</Option>
+            <Option value={1}>Empresa</Option>
+          </Select>
+        </Col>
+      </Row>
+    </>
+  )
+>>>>>>> Stashed changes
 }
 export default ReportAccountsReceivableFilters
--- src/pages/reports/reportAccountsReceivable/reportAccountsReceivableIndex.js
+++ src/pages/reports/reportAccountsReceivable/reportAccountsReceivableIndex.js
@@ -1,53 +1,54 @@
-import React,{useState} from 'react'
-import GenericTable from "../../../components/genericTable";
-import {Button, Divider, Popconfirm, Popover, Tag} from "antd";
-import MoreOutlined from "@ant-design/icons/lib/icons/MoreOutlined";
+import React, { useState } from 'react'
+import GenericTable from '../../../components/genericTable'
+import { Button, Divider, Popconfirm, Popover, Tag } from 'antd'
+import MoreOutlined from '@ant-design/icons/lib/icons/MoreOutlined'
+import ReportAccountsReceivableFilters from './components/reportAccountsReceivableFilters'
+import HeaderPage from '../../../components/HeaderPage'
 
 function ReportAccountsReceivable() {
-  
   const [loading, setLoading] = useState(false)
-  const [dataSource,setDataSource]= useState([])
-  
+  const [dataSource, setDataSource] = useState([])
+
   const data = [
     {
-      id : 1,
-      client_name: "Luis de leon",
+      id: 1,
+      client_name: 'Luis de leon',
       client_type: 1,
-      amount: "5000.00",
+      amount: '5000.00',
       bill_date: '11/02/2021',
-      payment_date:  '10/12/2021',
+      payment_date: '10/12/2021',
       payment_made_date: '10/12/2021',
       situation: 1,
-      observation: "Cliente nuevo etc.... 1234567890"
+      observation: 'Cliente nuevo etc.... 1234567890',
     },
     {
-      id : 3,
-      client_name: "Luis de leon",
+      id: 3,
+      client_name: 'Luis de leon',
       client_type: 2,
-      amount: "5000.00",
+      amount: '5000.00',
       bill_date: '11/02/2021',
-      payment_date:  '10/12/2021',
+      payment_date: '10/12/2021',
       payment_made_date: '10/12/2021',
       situation: 1,
-      observation: "Cliente nuevo etc.... 1234567890"
+      observation: 'Cliente nuevo etc.... 1234567890',
     },
     {
-      id : 3,
-      client_name: "Luis de leon",
+      id: 3,
+      client_name: 'Luis de leon',
       client_type: 3,
-      amount: "5000.00",
+      amount: '5000.00',
       bill_date: '11/02/2021',
-      payment_date:  '10/12/2021',
+      payment_date: '10/12/2021',
       payment_made_date: '10/12/2021',
       situation: 1,
-      observation: "Cliente nuevo etc.... 1234567890"
-    }
+      observation: 'Cliente nuevo etc.... 1234567890',
+    },
   ]
-  
+
   const columns = [
     {
       title: 'Cliente',
-      
+
       dataIndex: 'client_name', // Field that is goint to be rendered
       key: 'client_name',
       render: text => <span>{text}</span>,
@@ -82,18 +83,16 @@ function ReportAccountsReceivable() {
       key: 'situation',
       render: text => (
         <span>
-        {text === 1 ? (
-          <Tag color='#87d068'>PAGADO</Tag>
-        ) : text === 2 ? (
-          <Tag color='#f50'>SIN PAGAR</Tag>
-        )
-          : text === 3 ? (
-              <Tag color='#f50'>PAGO ATRASADO</Tag>
-            )
-            : (
-          ''
-        )}
-      </span>
+          {text === 1 ? (
+            <Tag color='#87d068'>PAGADO</Tag>
+          ) : text === 2 ? (
+            <Tag color='#f50'>SIN PAGAR</Tag>
+          ) : text === 3 ? (
+            <Tag color='#f50'>PAGO ATRASADO</Tag>
+          ) : (
+            ''
+          )}
+        </span>
       ),
     },
     {
@@ -108,49 +107,45 @@ function ReportAccountsReceivable() {
       key: 'id',
       render: (row, data) => (
         <span>
-        {
-          <Popover
-            placement='left'
-            style={{ zIndex: 'auto' }}
-            content={
-              <div>
-                <span className={'user-options-items'}>Editar</span>
-                <Divider
-                  className={'divider-enterprise-margins'}
-                  type={'horizontal'}
-                />
-                <Popconfirm
-                  title='Estas seguro de borrar el elemento selccionado?'
-                  okText='Si'
-                  cancelText='No'
-                >
-                  <span className={'user-options-items'}>Eliminar</span>
-                </Popconfirm>
-              </div>
-            }
-            trigger='click'
-          >
-            <Button shape={'circle'} className={'enterprise-settings-button'}>
-              <MoreOutlined />
-            </Button>
-          </Popover>
-        }
-      </span>
+          {
+            <Popover
+              placement='left'
+              style={{ zIndex: 'auto' }}
+              content={
+                <div>
+                  <span className={'user-options-items'}>Editar</span>
+                  <Divider
+                    className={'divider-enterprise-margins'}
+                    type={'horizontal'}
+                  />
+                  <Popconfirm
+                    title='Estas seguro de borrar el elemento selccionado?'
+                    okText='Si'
+                    cancelText='No'
+                  >
+                    <span className={'user-options-items'}>Eliminar</span>
+                  </Popconfirm>
+                </div>
+              }
+              trigger='click'
+            >
+              <Button shape={'circle'} className={'enterprise-settings-button'}>
+                <MoreOutlined />
+              </Button>
+            </Popover>
+          }
+        </span>
       ),
     },
   ]
 
-  
-  
-  return(<>
-    
-    <GenericTable
-      
-      data={data}
-      loading={loading}
-      columns={columns}
-    />
-  </>)
+  return (
+    <>
+      <HeaderPage title={'Reporte - Cuentas por cobrar'} />
+      <ReportAccountsReceivableFilters />
+      <GenericTable data={data} loading={loading} columns={columns} />
+    </>
+  )
 }
 
 export default ReportAccountsReceivable
