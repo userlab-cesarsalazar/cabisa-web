db08300a672b7a3a664ec28eb714f6064fc1b907
Luis de leon <luisdeleon_525@hotmail.com>
2021-02-24 18:29:33 -0600

    update
    update

--- src/pages/reports/reportAccountsReceivable/components/reportAccountsReceivableFilters.js
+++ src/pages/reports/reportAccountsReceivable/components/reportAccountsReceivableFilters.js
@@ -7,63 +7,6 @@ const { Option } = Select
 const { RangePicker } = DatePicker
 
 function ReportAccountsReceivableFilters() {
-<<<<<<< Updated upstream
-  
-  return(
-    <>
-    <Row gutter={16}>
-    <Col xs={6} sm={6} md={6} lg={6}>
-      <Search
-        prefix={<SearchOutlined className={'cabisa-table-search-icon'} />}
-        placeholder='Presiona enter para buscar'
-        className={'cabisa-table-search customSearch'}
-        size={'large'}
-      />
-    </Col>
-    <Col xs={6} sm={6} md={6} lg={6}>
-      <DatePicker
-        style={{ width: '100%', height: '40px', borderRadius: '8px' }}
-      />
-    </Col>
-    <Col xs={6} sm={6} md={6} lg={6}>
-      <Select
-        showSearch
-        className={'single-select'}
-        placeholder={'Tipo cliente'}
-        size={'large'}
-        style={{ width: '100%', height: '40px' }}
-        getPopupContainer={trigger => trigger.parentNode}
-      >
-        <Option value={0}>Persona Individual</Option>
-        <Option value={1}>Empresa</Option>
-      </Select>
-    </Col>
-    <Col xs={6} sm={6} md={6} lg={6}>
-      <DatePicker placeholder={'Fecha de Facturacion'}/>
-    </Col>
-    <Col xs={6} sm={6} md={6} lg={6}>
-      <DatePicker placeholder={'Fecha de Pago'}/>
-    </Col>
-    <Col xs={6} sm={6} md={6} lg={6}>
-      <DatePicker placeholder={'Fecha de Pago realizado'}/>
-    </Col>
-    <Col xs={6} sm={6} md={6} lg={6}>
-      <Select
-        showSearch
-        className={'single-select'}
-        placeholder={'Situacion'}
-        size={'large'}
-        style={{ width: '100%', height: '40px' }}
-        getPopupContainer={trigger => trigger.parentNode}
-      >
-        <Option value={0}>Persona Individual</Option>
-        <Option value={1}>Empresa</Option>
-      </Select>
-    </Col>
-  </Row>
-      </>)
-    
-=======
   return (
     <>
       <Row gutter={16}>
@@ -120,6 +63,5 @@ function ReportAccountsReceivableFilters() {
       </Row>
     </>
   )
->>>>>>> Stashed changes
 }
 export default ReportAccountsReceivableFilters
