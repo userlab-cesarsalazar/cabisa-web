99467c0cee3a913a5d1f893b9443865210788b89
userlab-luisdeleon <59143067+userlab-luisdeleon@users.noreply.github.com>
2021-04-21 11:03:04 -0600

    remove keys
    remove keys

--- src/commons/credentials.js
+++ src/commons/credentials.js
@@ -4,10 +4,6 @@ const develop = {
   usersUrl: 'https://oj41d9rbdi.execute-api.us-east-1.amazonaws.com/dev/users',
   inventoryUrl:
     'https://gnemhrle04.execute-api.us-east-1.amazonaws.com/dev/products',
-  awsAccessKeyId: 'AKIA5OM7ZNT7LL7EHO5R',
-  awsSecretAccessKey: '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',
-  awsRegion: 'us-east-1',
-  awsUserPoolId: 'us-east-1_buevGtRrC',
 }
 
 const enviroment = env => {
--- src/pages/users/usersIndex.js
+++ src/pages/users/usersIndex.js
@@ -69,20 +69,6 @@ function Users(props) {
     try {
       setLoading(true)
 
-      AWS.config.update({
-        accessKeyId: stage.awsAccessKeyId,
-        secretAccessKey: stage.awsSecretAccessKey,
-        region: stage.awsRegion,
-      })
-      const cognito = new AWS.CognitoIdentityServiceProvider()
-
-      await cognito
-        .adminDeleteUser({
-          UserPoolId: stage.awsUserPoolId,
-          Username: data.email,
-        })
-        .promise()
-
       UsersSrc.deleteUser({ id: data.id })
         .then(_ => {
           message.success('Usuario eliminado')
@@ -110,27 +96,7 @@ function Users(props) {
     try {
       setVisible(false)
       setShowPermissions(false)
-      let _changePassword = data.password
       delete data.password
-
-      if (_changePassword.replace(/\s/g, '').length > 0) {
-        AWS.config.update({
-          accessKeyId: stage.awsAccessKeyId,
-          secretAccessKey: stage.awsSecretAccessKey,
-          region: stage.awsRegion,
-        })
-        const cognito = new AWS.CognitoIdentityServiceProvider()
-
-        await cognito
-          .adminSetUserPassword({
-            Password: _changePassword,
-            UserPoolId: stage.awsUserPoolId,
-            Username: data.email,
-            Permanent: true,
-          })
-          .promise()
-      }
-
       UsersSrc.updateUser(data)
         .then(_ => {
           message.success('Informacion actualizada')
