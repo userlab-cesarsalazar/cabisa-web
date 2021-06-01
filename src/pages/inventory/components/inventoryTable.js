4f768deb7ffb1cf5887af6473ce3abdec7236e3a
rzlams <lamsarmiento@gmail.com>
2021-05-31 21:44:03 -0400

    products

--- src/pages/inventory/components/inventoryTable.js
+++ src/pages/inventory/components/inventoryTable.js
@@ -1,20 +1,26 @@
 import React from 'react'
-import { Table, Col, Input, Button, Row, Card, Tag, Select } from 'antd'
+import {
+  Table,
+  Col,
+  Input,
+  Button,
+  Row,
+  Card,
+  Tag as AntTag,
+  Select,
+} from 'antd'
 import SearchOutlined from '@ant-design/icons/lib/icons/SearchOutlined'
-import { validatePermissions } from '../../../utils/Utils'
+import { validatePermissions } from '../../../utils'
 import { Cache } from 'aws-amplify'
+import Tag from '../../../components/Tag'
 
 const { Search } = Input
 const { Option } = Select
 
 function InventoryTable(props) {
-  const getFilteredData = data => {
-    props.handlerTextSearch(data)
-  }
+  const getFilteredData = data => props.handlerTextSearch(data)
 
-  const filterCategory = data => {
-    props.handlerCategoryService(data)
-  }
+  const filterCategory = data => props.handlerCategorySearch(data)
 
   return (
     <>
@@ -26,7 +32,7 @@ function InventoryTable(props) {
             className={'cabisa-table-search customSearch'}
             style={{ width: '100%', height: '40px' }}
             size={'large'}
-            onSearch={e => getFilteredData(e)}
+            onSearch={getFilteredData}
           />
         </Col>
         <Col
@@ -37,7 +43,7 @@ function InventoryTable(props) {
           className={props.warehouse ? 'stash-component' : ''}
         >
           <Select
-            defaultValue={99}
+            defaultValue={''}
             className={'single-select'}
             placeholder={'Categoria'}
             size={'large'}
@@ -45,16 +51,14 @@ function InventoryTable(props) {
             getPopupContainer={trigger => trigger.parentNode}
             onChange={value => filterCategory(value)}
           >
-            <Option value={99}>
-              <Tag color='cyan'>Todo</Tag>
+            <Option value={''}>
+              <AntTag color='cyan'>Todo</AntTag>
             </Option>
-
-            <Option value={2}>
-              <Tag color='blue'>Equipo</Tag>
-            </Option>
-            <Option value={3}>
-              <Tag color='orange'>Repuesto</Tag>
-            </Option>
+            {props.productCategoriesList?.map(value => (
+              <Option key={value} value={value}>
+                <Tag type='productCategories' value={value} />
+              </Option>
+            ))}
           </Select>
         </Col>
         <Col xs={6} sm={6} md={6} lg={6} style={{ textAlign: 'right' }}>
