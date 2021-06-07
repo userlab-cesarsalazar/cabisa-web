ca4e4a455aa9185078611269bf058f020313fc17
userlab-luismoreno <69315507+userlab-luismoreno@users.noreply.github.com>
2021-06-07 16:09:37 -0400

    Facturacion (#17)
    * billing
    
    * filters
    
    * fix init filters
    
    Co-authored-by: rzlams <lamsarmiento@gmail.com>

--- src/pages/billing/components/detailBilling.js
+++ src/pages/billing/components/detailBilling.js
@@ -2,43 +2,30 @@ import React from 'react'
 import { Card, Drawer } from 'antd'
 import BillingFields from './billingFields'
 function DetailBilling(props) {
-  const dataDummyDetails = [
-    {
-      quantity: '1',
-      type_service: 1,
-      description: 'Cabina',
-      unit_price: '1000',
-      sub_total: 1000,
-    },
-    {
-      quantity: '3',
-      type_service: 0,
-      description: 'Pipa extractora',
-      unit_price: '1000',
-      sub_total: 3000,
-    },
-    {
-      quantity: '1',
-      type_service: 2,
-      description: 'Aseroria',
-      unit_price: '1000',
-      sub_total: 1000,
-    },
-  ]
   return (
     <Drawer
       placement='right'
       closable={false}
       onClose={props.closable}
       visible={props.visible}
-      width={850}
+      width='70%'
     >
       <Card className={'card-border-radius margin-top-15'}>
         <BillingFields
           visible={props.visible}
           edit={true}
-          data={props.editData}
-          dataDetail={dataDummyDetails}
+          loading={props.loading}
+          setLoading={props.setLoading}
+          data={props.data}
+          productsData={props.productsData}
+          paymentMethodsOptionsList={props.paymentMethodsOptionsList}
+          stakeholderTypesOptionsList={props.stakeholderTypesOptionsList}
+          discountInputValue={props.discountInputValue}
+          handleDiscountChange={() => {}}
+          handleChange={() => {}}
+          handleSearchStakeholder={() => {}}
+          handleSearchProduct={() => {}}
+          handleSearchProject={() => {}}
         />
       </Card>
     </Drawer>
