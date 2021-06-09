a5a5a275ec0f6c272a32e30d163e7be30f11e6a1
userlab-luismoreno <69315507+userlab-luismoreno@users.noreply.github.com>
2021-06-09 11:14:27 -0400

    Compras (movimientos) (#16)
    * add purchases
    
    * billing init filters
    
    Co-authored-by: rzlams <lamsarmiento@gmail.com>

--- src/pages/inventory/inventorySrc.js
+++ src/pages/inventory/inventorySrc.js
@@ -2,12 +2,22 @@ import api from '../../commons/api'
 import { stage } from '../../commons/credentials'
 
 const urlProduct = stage.productUrl
+const urlPurchase = stage.purchaseUrl
 const urlService = stage.serviceUrl
+const urlStakeholder = stage.stakeholderUrl
+
+const getPurchases = params => api.get(urlPurchase, { ...params })
+const createPurchase = data => api.post(urlPurchase, data)
+const cancelPurchase = data => api.put(`${urlPurchase}/cancel`, data)
 
 const getProducts = params => api.get(urlProduct, { ...params })
 const getProductsStatus = () => api.get(`${urlProduct}-status`)
 const getProductsCategories = () => api.get(`${urlProduct}-categories`)
 const getProductsTaxes = () => api.get(`${urlProduct}-taxes`)
+const getProductsOptions = params =>
+  api.get(`${urlProduct}-options`, { ...params })
+const getStakeholdersOptions = params =>
+  api.get(`${urlStakeholder}-options`, { ...params })
 const createProduct = _products => api.post(urlProduct, _products)
 const updateProduct = _products => api.put(urlProduct, _products)
 const deleteProduct = _products => api.remove(urlProduct, _products)
@@ -19,10 +29,15 @@ const updateService = _services => api.put(urlService, _services)
 const deleteService = _services => api.remove(urlService, _services)
 
 const InventorySrc = {
+  getPurchases,
+  createPurchase,
+  cancelPurchase,
   getProducts,
   getProductsCategories,
+  getProductsOptions,
   getProductsStatus,
   getProductsTaxes,
+  getStakeholdersOptions,
   createProduct,
   updateProduct,
   deleteProduct,
