2bd15f602f278daa70b9b501d3e26b70468c807a
rzlams <lamsarmiento@gmail.com>
2021-06-09 20:04:46 -0400

    update endpoints

--- src/commons/credentials.js
+++ src/commons/credentials.js
@@ -2,14 +2,21 @@ const develop = {
   clientsUrl:
     'https://z8whyqlcgl.execute-api.us-east-1.amazonaws.com/dev/clients',
   usersUrl: 'https://oj41d9rbdi.execute-api.us-east-1.amazonaws.com/dev/users',
-  productUrl: 'http://127.0.0.1:5500/products',
-  // 'https://gnemhrle04.execute-api.us-east-1.amazonaws.com/dev/products',
-  projectUrl: 'http://127.0.0.1:4000/projects',
-  purchaseUrl: 'http://127.0.0.1:3000/purchases',
-  saleUrl: 'http://127.0.0.1:4000/sales',
-  serviceUrl: 'http://127.0.0.1:3000/services',
-  stakeholderUrl: 'http://127.0.0.1:5000/stakeholders',
-  invoiceUrl: 'http://127.0.0.1:3000/invoices',
+  productUrl:
+    'https://pgs3yitu93.execute-api.us-east-1.amazonaws.com/dev/products',
+  projectUrl:
+    'https://ekxeyrhvhh.execute-api.us-east-1.amazonaws.com/dev/projects',
+  purchaseUrl:
+    'https://ezx8vhace8.execute-api.us-east-1.amazonaws.com/dev/purchases',
+  saleUrl: 'https://v8z8ctg7ta.execute-api.us-east-1.amazonaws.com/dev/sales',
+  serviceUrl:
+    'https://ac5n79hsk1.execute-api.us-east-1.amazonaws.com/dev/services',
+  stakeholderUrl:
+    'https://v5xx70s4r6.execute-api.us-east-1.amazonaws.com/dev/stakeholders',
+  invoiceUrl:
+    'https://auf9sd3ft5.execute-api.us-east-1.amazonaws.com/dev/invoices',
+  inventoryMovementUrl:
+    'https://bjj4etorc5.execute-api.us-east-1.amazonaws.com/dev/inventory-movements',
 }
 
 const enviroment = env => {
