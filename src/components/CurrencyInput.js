f0bec2c821fb69be09a8badd99ff6ac4f1b0e822
rzlams <lamsarmiento@gmail.com>
2021-08-17 15:58:17 -0400

    currency input returning string

--- src/components/CurrencyInput.js
+++ src/components/CurrencyInput.js
@@ -1,125 +1,36 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Input } from 'antd'
-/*
-<CurrencyInput
-  placeholder={'Escribir TEST'}
-  size={'large'}
-  style={{ height: '40px' }}
-  value={test}
-  onChange={value => setTest(value)}
-  currencyFormat='en-US'
-  minimumFractionDigits={2}
-  maximumFractionDigits={2}
-  currencyPrefix={true}
-  currencySuffix={false}
-/>
-*/
+import { numberFormat } from '../utils'
 
-const numberFormat = ({
-  currencyFormat,
-  minimumFractionDigits,
-  maximumFractionDigits,
-} = {}) => {
-  // currencyFormat = 'de-DE' usa punto para miles y coma para decimales
-  // currencyFormat = 'en-US' usa coma para miles y punto para decimales
-  const fractionSeparator = currencyFormat === 'de-DE' ? ',' : '.'
-  const groupSeparator = currencyFormat === 'de-DE' ? '.' : ','
-
-  const toRegExp = function (val) {
-    const escaped = val.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&')
-    return new RegExp(escaped, 'g')
-  }
-
-  const getFormattedValue = (value, minimumFractionDigitsOnBlur = 0) => {
-    if (!value || isNaN(Number(value))) return value
-
-    return new Intl.NumberFormat(currencyFormat, {
-      minimumFractionDigits: minimumFractionDigitsOnBlur,
-      maximumFractionDigits,
-    }).format(value)
-  }
-
-  const getValue = formattedValue => {
-    const result = String(formattedValue)
-      .replace(toRegExp(groupSeparator), '')
-      .replace(fractionSeparator, groupSeparator)
-
-    return Number(result)
-  }
-
-  return {
-    getFormattedValue,
-
-    getValue,
-
-    handleChange: rawValue => {
-      const allowedCharacters = [
-        '0',
-        '1',
-        '2',
-        '3',
-        '4',
-        '5',
-        '6',
-        '7',
-        '8',
-        '9',
-        fractionSeparator,
-        groupSeparator,
-      ]
-
-      if (!rawValue && rawValue !== '') return { value: 0, formattedvalue: '' }
-
-      const qtyOfFractionSeparators = String(rawValue)
-        .split('')
-        .reduce((r, v) => (r += v === fractionSeparator ? 1 : 0), 0)
-
-      if (qtyOfFractionSeparators > 1)
-        return { value: null, formattedvalue: null }
-
-      const validatedValue = String(rawValue)
-        .split('')
-        .reduce(
-          (r, v) => `${r}${allowedCharacters.some(ac => ac === v) ? v : ''}`,
-          ''
-        )
-      const decimals = validatedValue.split(fractionSeparator)[1]
-      const lastValueChar = validatedValue[validatedValue.length - 1]
-
-      const value = getValue(validatedValue)
-      const formattedvalue =
-        (lastValueChar === fractionSeparator || lastValueChar === '0') &&
-        decimals?.length <= 2
-          ? validatedValue
-          : getFormattedValue(value)
-
-      return { value, formattedvalue }
-    },
-
-    handleBlur: rawValue => {
-      const value = getValue(rawValue)
-      const formattedvalue = getFormattedValue(value, minimumFractionDigits)
-
-      return formattedvalue
-    },
-  }
-}
-
-function CurrencyInput({
-  currencyPrefix = true,
+const CurrencyInput = ({
+  showPrefix = false,
+  showSuffix = false,
   currencySymbol = 'Q',
   currencyFormat = 'de-DE',
-  minimumFractionDigits = 2,
-  maximumFractionDigits = 2,
+  fractionDigits = 2,
   ...props
-}) {
+}) => {
   const { getFormattedValue, handleChange, handleBlur } = numberFormat({
     currencyFormat,
-    minimumFractionDigits,
-    maximumFractionDigits,
+    fractionDigits,
   })
 
-  const [_inputValue, _setInputValue] = useState(getFormattedValue(props.value))
+  const [_inputValue, _setInputValue] = useState(null)
+
+  useEffect(() => {
+    const value =
+      typeof props.value === 'number'
+        ? getFormattedValue(props.value)
+        : props.value
+
+    _setInputValue(value)
+  }, [props.value, getFormattedValue])
+
+  useEffect(function cleanUp() {
+    return () => {
+      _setInputValue(null)
+    }
+  }, [])
 
   const _handleChange = e => {
     if (!props.onChange) return
@@ -128,24 +39,25 @@ function CurrencyInput({
       throw new Error('The prop onChange must be a function')
 
     const rawValue = e?.target?.value || ''
-    const { formattedvalue, value } = handleChange(rawValue)
+    const { formattedValue } = handleChange(rawValue)
 
-    _setInputValue(formattedvalue ?? _inputValue)
-    props.onChange(value ?? props.value)
+    props.onChange(formattedValue)
   }
 
   const _handleBlur = e => {
     const rawValue = e?.target?.value || ''
+    const formattedValue = handleBlur(rawValue)
 
-    const formattedvalue = handleBlur(rawValue)
-    _setInputValue(formattedvalue)
+    typeof props.onChange === 'function' && props.onChange(formattedValue)
+    typeof props.onBlur === 'function' && props.onBlur(formattedValue)
   }
 
   return (
     <Input
-      prefix={currencyPrefix ? <div>{currencySymbol}</div> : null}
-      suffix={props.currencySuffix ? <div>{currencySymbol}</div> : null}
+      prefix={showPrefix ? <div>{currencySymbol}</div> : null}
+      suffix={showSuffix ? <div>{currencySymbol}</div> : null}
       {...props}
+      type='text'
       value={_inputValue}
       onChange={_handleChange}
       onBlur={_handleBlur}
