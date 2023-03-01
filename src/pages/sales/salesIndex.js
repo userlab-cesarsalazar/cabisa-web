f4843a6dd95fadaccfc56abf92af6f0e5193cc81
userlab-luisdeleon <59143067+userlab-luisdeleon@users.noreply.github.com>
2023-03-01 09:53:52 -0600

    Permisos a editar ventas a vendedores (#78)

--- src/pages/sales/salesIndex.js
+++ src/pages/sales/salesIndex.js
@@ -7,7 +7,7 @@ import { actions, permissions, roles } from '../../commons/types'
 function Sales() {
   const can = validatePermissions(permissions.VENTAS)
   const canEditAndCreate = can(actions.CREATE) || can(actions.EDIT)
-  const isAdmin = validateRole(roles.ADMIN)
+  const isAdmin = validateRole(roles.ADMIN) || validateRole(roles.SELLS)
 
   return (
     <>
